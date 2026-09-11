cb25ccbd80029c4d93d82a8702fa122d1296d3e7
TebaFirasAMM <senatf2003@gmail.com>
2026-09-11 17:23:39 +0300

    Fix indentation for risk score conditionals

--- main.py
+++ main.py
@@ -232,7 +232,7 @@ if run_btn:
                 st.success(f"🟢 Safe Condition & Under Control (Risk Rate: {risk_score}%)")
             elif 30 <= risk_score < 70:
                 st.warning(f"🟡 Moderate Alert - Potential Risk Requiring Follow-up (Risk Rate: {risk_score}%)")
- else:
+            else:
                 st.error(f"🔴 High Risk Warning - Immediate Medical Intervention Required (Risk Rate: {risk_score}%)")
         
         st.markdown("<br>", unsafe_allow_html=True)
